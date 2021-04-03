6e72616f07ce5a3c5a332d4791587d3ad21d55b7
Sergio Herrera <herrera.se1992@gmail.com>
2021-04-03 20:08:54 -0300

    Aplicando group by

--- top100.sql
+++ top100.sql
@@ -10,11 +10,21 @@ CREATE TABLE Reparto (Peli_id INT, Nombre_Actor VARCHAR(100), FOREIGN KEY(Peli_I
 \copy peliculas FROM '/mnt/c/users/bazz/desktop/SQL/proyectos/Desafio_02/top_100/peliculas.csv' csv header;
 \copy reparto FROM '/mnt/c/users/bazz/desktop/SQL/proyectos/Desafio_02/top_100/reparto.csv' csv;
 
-4. Listar todos los actores que aparecen en la película "Titanic", indicando el título de la película,
-año de estreno, director y todo el reparto. (0.5 puntos)
-5. Listar los titulos de las películas donde actúe Harrison Ford.(0.5 puntos)
-6. Listar los 10 directores mas populares, indicando su nombre y cuántas películas aparecen en el
-top 100.(1 puntos)
+--4. Listar todos los actores que aparecen en la película "Titanic", indicando el título de la película,
+--año de estreno, director y todo el reparto. (0.5 puntos)
+
+SELECT * FROM peliculas INNER JOIN reparto ON peliculas.id = reparto.peli_id WHERE peliculas.pelicula = 'Titanic';
+
+
+--5. Listar los titulos de las películas donde actúe Harrison Ford.(0.5 puntos)
+
+SELECT pelicula FROM peliculas INNER JOIN reparto ON peliculas.id = reparto.peli_id WHERE reparto.Nombre_Actor = 'Harrison Ford';
+
+--6. Listar los 10 directores mas populares, indicando su nombre y cuántas películas aparecen en el
+--top 100.(1 puntos)
+
+SELECT director, COUNT(*) AS TOTAL_PELICULAS FROM peliculas GROUP BY director ORDER  BY TOTAL_PELICULAS DESC LIMIT 10;
+
 7. Indicar cuantos actores distintos hay (1 puntos)
 8. Indicar las películas estrenadas entre los años 1990 y 1999 (ambos incluidos) ordenadas por
 título de manera ascendente.(1 punto)
